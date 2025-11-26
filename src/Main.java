fe2a00302933be7440d50152c21493c20d08b609
student <student@ur.edu.pl>
2025-11-26 19:34:22 +0100

    data base connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import DataBase.DataBaseConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+            try (Connection conn = DataBaseConnection.getConnection()) {
+                if (conn != null) {
+                    System.out.println("Połączenie działa!");
+                }
+            } catch (SQLException e) {
+                System.out.println("Błąd połączenia ");
+                e.printStackTrace();
+            }
+
+
+
     }
 }
